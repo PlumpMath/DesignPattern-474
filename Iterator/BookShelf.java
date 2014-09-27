f301d12b47d4066fef575ac62a5c902a0efd10c1
TakanoriMatsumoto <matscube@TakanoriMatsumoto-no-MacBook-Pro-2.local>
2014-09-28 04:12:41 +0900

    Suggested answer.

--- Iterator/BookShelf.java
+++ Iterator/BookShelf.java
@@ -2,19 +2,17 @@ import java.util.ArrayList;
 
 public class BookShelf implements Aggregate {
 	private ArrayList<Book> books;
-	private int last = 0;
-	public BookShelf() {
-		this.books = new ArrayList<Book>();
+	public BookShelf(int initialsize) {
+		this.books = new ArrayList<Book>(initialsize);
 	}
 	public Book getBookAt(int index) {
 		return books.get(index);
 	}
 	public void appendBook(Book book) {
 		books.add(book);
-		last++;
 	}
 	public int getLength() {
-		return last;
+		return books.size();
 	}
 	public Iterator iterator() {
 		return new BookShelfIterator(this);
